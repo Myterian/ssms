63965a6ab58a461231d9bbb5125cf84165a15023
Myterian <chris.bachler@aol.de>
2021-12-02 23:24:31 +0100

    Add files via upload

---
+++ SSMS.cs
@@ -0,0 +1,219 @@
+// Modified version of Keijiro Streak effect
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace byteslider.PostProcessing.SSMS
+{
+    [PostProcess(typeof(SSMSRenderer), PostProcessEvent.BeforeStack, "byteslider/Screen Space Multi Scattering")]
+	public class SSMS : PostProcessEffectSettings 
+    {
+        [ Header("Multi Scatter Fog") ]
+
+        [ Tooltip( "How far off in the distance the effect should start in world units" ) ]
+        public FloatParameter startDistance = new FloatParameter { value = 15f };
+
+        [ Tooltip( "How dense the fog is. Affects how much the light gets scattered" ) ]
+        [Range(0, 5)] public FloatParameter density = new FloatParameter { value = 3f };
+
+        [ Tooltip( "Controls how much the fog is overlayed on the scene. Helps to blend the fog in or out, which works best with a density of 0" ) ]
+        [Range(0, 1)] public FloatParameter blend = new FloatParameter { value = 1f };
+
+        [ Tooltip( "Controls how much light is preserved. Less values mean more energy loss of the light" ) ]
+        [Range(0, 1)] public FloatParameter intensity = new FloatParameter { value = 0.05f };
+
+        [ Tooltip( "Fog tint. Brightness also contributes to intensity" ) ]
+        [ColorUsage(false)] public ColorParameter tint = new ColorParameter { value = new Color( 0.262f, 0.298f, 0.33f ) };
+        
+        [ Header("Performance") ]
+        
+        [ Tooltip( "Should the effect use a high quality blur or not" ) ]
+        public BoolParameter highQuality = new BoolParameter { value = true };
+
+        [ Tooltip( "Uses half as many samples as the effect normally would" ) ]
+        public BoolParameter fastMode = new BoolParameter { value = false };
+        
+        [UnityEngine.Rendering.PostProcessing.Min(0), Tooltip( "Custom setting for how many samples should be used" )] 
+        public IntParameter customQuality = new IntParameter { value = 0 };
+
+        #if UNITY_EDITOR
+        [ Header("Debug") ]
+
+        [ Tooltip( "What pass should be reviewed. 1 is prefilter, 2 is downsample, 3 is upsample" ) ]
+        public IntParameter debugPass = new IntParameter { value = 0 };
+        #endif
+    }
+
+
+    public sealed class SSMSRenderer : PostProcessEffectRenderer<SSMS>
+	{
+        // Fetch shader variable ids
+        static class ShaderPropertyID
+        {
+            internal static readonly int StartDistance = Shader.PropertyToID("_StartDistance");
+            internal static readonly int Density = Shader.PropertyToID("_Density");
+            internal static readonly int Blend = Shader.PropertyToID("_Blend");
+            internal static readonly int Intensity = Shader.PropertyToID("_Intensity");
+            internal static readonly int Color = Shader.PropertyToID("_Color");
+            internal static readonly int HighTex = Shader.PropertyToID("_HighTex");
+        }
+
+        // Future proofing. Avoids large screen resolutions (64k) to go overboard with samples
+        const int MaxMipLevel = 16;
+
+        int[] _rtMipDown;
+        int[] _rtMipUp;
+        
+        public override void Init()
+        {
+            _rtMipDown = new int[MaxMipLevel];
+            _rtMipUp = new int[MaxMipLevel];
+
+            for (var i = 0; i < MaxMipLevel; i++) {
+                _rtMipDown[i] = Shader.PropertyToID("_MipDown" + i);
+                _rtMipUp[i] = Shader.PropertyToID("_MipUp" + i);
+            }
+        }
+
+        // Fog Renderer
+        public override void Render(PostProcessRenderContext context)
+        {
+            var cmd = context.command;
+            cmd.BeginSample("SSMS");
+
+            // Shader uniforms
+            var sheet = context.propertySheets.Get(Shader.Find("Hidden/PostProcessing/SSMS"));
+            sheet.properties.SetFloat(ShaderPropertyID.StartDistance, settings.startDistance);
+            sheet.properties.SetFloat(ShaderPropertyID.Density, settings.density);
+            sheet.properties.SetColor(ShaderPropertyID.Color, settings.tint);
+
+            // Easier to control transition for blend. Otherwise you need to fiddle with .997 - .999 values
+            float _blend = Mathf.Log( Mathf.Pow( 2.71828f, settings.blend ) );
+            sheet.properties.SetFloat(ShaderPropertyID.Blend, _blend);
+
+            if( settings.highQuality )
+                sheet.EnableKeyword("HIGH_QUALITY_UPSAMPLE");
+            else
+                sheet.DisableKeyword("HIGH_QUALITY_UPSAMPLE");
+
+            // Figure out how many iterations we need
+            // Figure out which side of the screen is larger. For all our portrait mode lovers
+            int halfWidth = Mathf.FloorToInt( context.screenWidth / 2f );
+            int halfHeight = Mathf.FloorToInt( context.screenHeight / 2f );
+            int maxScreenResolution = Mathf.Max( halfWidth, halfHeight );
+
+            //Figure out how many mip iterations we would ideally need to reach our screens resolution
+            float log = Mathf.Log( maxScreenResolution, 2f );
+            int exponent = Mathf.FloorToInt( log );
+            exponent = Mathf.Clamp( exponent, 1, MaxMipLevel );
+
+            // Set up how many mip iteration we will actually use. By default, all of them
+            int iteration = exponent;
+
+            if( settings.customQuality != 0 )
+                iteration = settings.customQuality;
+            else if( settings.fastMode )
+                iteration = exponent / 2;
+
+            iteration = Mathf.Clamp( iteration, 1, MaxMipLevel );
+
+            // Adjusts the intensity for the sample (<- iterations) count. Lower samples usually return a darker image than higher samples on the same intensity
+            float _intensity = ( settings.intensity * ((float)exponent / (float)iteration) );
+            sheet.properties.SetFloat( ShaderPropertyID.Intensity, _intensity );
+
+            // float _density = settings.density * ((float)iteration / (float)exponent);
+            // sheet.properties.SetFloat( ShaderPropertyID.Density, _density );
+
+            // Accounts for custom quality setting. With lower iterations than desired, the mips can't be sampled step by step like they normally would. That would result in less blur, 
+            // because the sample resolution wouldn't go low enough. We want about the same amount of blur like normal, but with less samples. That's why we spread the samples out, to get 
+            // as much mip coverage as we can, by skipping a step in the mip pyramid when we can.
+            // The first sample will always be half screen resolution and the last sample will always be the desired smallest screen resolution
+            float mipSkip = (float)exponent - 2f / (float)iteration - 2f;
+            int levelOffset = 1;
+
+            // Build the MIP pyramid.
+            var level = 1;
+            for (; level < iteration; level++) 
+            {
+                Vector2Int screenSize = new Vector2Int( context.screenWidth / levelOffset, context.screenHeight / levelOffset );
+                context.GetScreenSpaceTemporaryRT( cmd, _rtMipDown[level], 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Default, FilterMode.Bilinear, screenSize.x, screenSize.y );
+
+                // First sample will be the pre filter
+                if( level == 1 )
+                {
+                    cmd.BlitFullscreenTriangle(context.source, _rtMipDown[level], sheet, 0);
+
+                    // Debug
+                    #if UNITY_EDITOR
+                    if( settings.debugPass == 1 )
+                    {
+                        cmd.BlitFullscreenTriangle(_rtMipDown[level], context.destination);
+                        cmd.ReleaseTemporaryRT(_rtMipDown[level]);
+                        return;
+                    }
+                    #endif
+                }
+                else
+                {
+                    cmd.BlitFullscreenTriangle(_rtMipDown[level - 1], _rtMipDown[level], sheet, 1);
+                    
+                    // Debug
+                    #if UNITY_EDITOR
+                    if( settings.debugPass == 2 )
+                    {
+                        cmd.ReleaseTemporaryRT(_rtMipDown[level - 1]);
+                    }
+                    #endif
+                }
+
+                // Calculating next screen resolution
+                levelOffset = Mathf.Clamp( level + Mathf.CeilToInt( mipSkip ), 1, exponent );
+            }
+
+            // Debug
+            #if UNITY_EDITOR
+            if( settings.debugPass == 2)
+            {
+                cmd.BlitFullscreenTriangle(_rtMipDown[level], context.destination);
+                cmd.ReleaseTemporaryRT(_rtMipDown[level]);
+                return;
+            }
+            #endif
+
+            // Upsample and combine.
+            var lastRT = _rtMipDown[--level];
+            for (; level >= 1; level--) 
+            {
+                Vector2Int screenSize = new Vector2Int( context.screenWidth / levelOffset, context.screenHeight / levelOffset );
+                context.GetScreenSpaceTemporaryRT( cmd, _rtMipUp[level], 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Default, FilterMode.Bilinear, screenSize.x, screenSize.y);
+                cmd.SetGlobalTexture(ShaderPropertyID.HighTex, _rtMipDown[level]);
+                cmd.BlitFullscreenTriangle(lastRT, _rtMipUp[level], sheet, 2);
+
+                cmd.ReleaseTemporaryRT(_rtMipDown[level]);
+                cmd.ReleaseTemporaryRT(lastRT);
+
+                lastRT = _rtMipUp[level];
+
+                // Calculating next screen resolution
+                levelOffset = ( Mathf.Max(level - levelOffset, 1));
+            }
+
+            // Debug
+            #if UNITY_EDITOR
+            if( settings.debugPass == 3)
+            {
+                cmd.BlitFullscreenTriangle(lastRT, context.destination);
+                cmd.ReleaseTemporaryRT(lastRT);
+                return;
+            }
+            #endif
+
+            // Final composition.
+            cmd.SetGlobalTexture(ShaderPropertyID.HighTex, context.source);
+            cmd.BlitFullscreenTriangle(lastRT, context.destination, sheet, 3);
+
+            // Cleaning up.
+            cmd.ReleaseTemporaryRT(lastRT);
+            cmd.EndSample("SSMS");
+        }
+	}
+}
---
+++ SSMS.shader
@@ -0,0 +1,159 @@
+﻿Shader "Hidden/PostProcessing/SSMS"
+{
+	HLSLINCLUDE
+
+	#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+    #pragma multi_compile __ HIGH_QUALITY_UPSAMPLE
+
+	TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+	TEXTURE2D_SAMPLER2D(_HighTex, sampler_HighTex);
+    TEXTURE2D_SAMPLER2D(_CameraDepthTexture, sampler_CameraDepthTexture);
+
+	float4 _MainTex_TexelSize;
+    float4 _CameraDepthTexture_TexelSize;
+	float _StartDistance;
+    float _Density;
+	float _Blend;
+	float _Intensity;
+	half3 _Color;
+
+    // Sample Depth with offet ( StartDistance ) and scale ( Density )
+    half Depth( float2 uv )
+    {
+        half depth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, sampler_CameraDepthTexture, uv));
+
+        // "Scale and offset" basically
+        depth = max( 0, depth - _StartDistance);
+        depth = saturate( depth - 0.99 * depth );
+
+        return depth;
+    }
+
+	// Prefilter: Apply depth as threshold to image.
+	half4 FragPrefilter(VaryingsDefault i) : SV_Target
+	{
+        float2 uv = i.texcoord;
+        // half depth = Depth(uv, false);
+
+        // Small box blur. Helps to remove ambient occlusion like effect from the fog
+        float4 d = _MainTex_TexelSize.xyxy * float4( -1, -1, 1, 1 ) * 10;
+        half3 c0 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xy).rgb * Depth(uv + d.xy); // -1, -1
+        half3 c1 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zy).rgb * Depth(uv + d.zy); //  1, -1
+        half3 c2 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xw).rgb * Depth(uv + d.xw); // -1,  1
+        half3 c3 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zw).rgb * Depth(uv + d.zw); //  1,  1
+        half3 c4 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv).rgb * Depth(uv);
+        half3 c = max( max( max( c0, c1), max( c2, c3 ) ), c4 );
+
+        // Greyscale
+        c = max(max(c.r, c.g), c.b);
+
+        return half4( c * _Color, 1 );
+	}
+
+	// Downsampler
+	half4 FragDownsample(VaryingsDefault i) : SV_Target
+	{
+        float2 uv = i.texcoord;
+        float4 d = _MainTex_TexelSize.xyxy * float4(-1, -1, 1, 1);
+
+        // Simple box blur
+        half3 c0 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xy).rgb; // -1, -1
+		half3 c1 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zy).rgb; //  1, -1
+		half3 c2 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xw).rgb; // -1,  1
+		half3 c3 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zw).rgb; //  1,  1
+        half3 c4 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv).rgb;
+
+        // Helps to remove ambient occlusion like effect from the fog
+        half3 c = ( max( c0, c4) + max( c1, c4) + max( c2, c4) + max( c3, c4) ) / 4 ;
+
+        return half4( c, 1 );
+	}
+
+	// Upsampler
+	half4 FragUpsample(VaryingsDefault i) : SV_Target
+	{
+        float2 uv = i.texcoord;
+        half3 highTex = SAMPLE_TEXTURE2D(_HighTex, sampler_HighTex, uv).rgb;
+
+        #if HIGH_QUALITY_UPSAMPLE
+
+            // High quality weighted blur
+            half3 c4 = 0;
+            float4 d = _MainTex_TexelSize.xyxy * float4(1, 1, -1, 0);
+
+            c4  = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv - d.xy).rgb;     //  1,  0
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv - d.wy).rgb * 2; // -1,  0
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv - d.zy).rgb;     // -1,  0
+
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zw).rgb * 2; //  1,  0
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv       ).rgb * 4;
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xw).rgb * 2; // -1,  0
+
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zy).rgb;     //  1, -1
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.wy).rgb * 2; //  0, -1
+            c4 += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xy).rgb;     // -1, -1
+
+            half3 c = c4 / 16;
+            return half4( highTex + c * ( 1 + _Density ), 1) / ( 1 + ( _Density * 0.735));
+
+        #else
+
+            // Box Blur
+            float4 d = _MainTex_TexelSize.xyxy * float4(-1, -1, 1, 1);
+            half3 c0 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xy).rgb; // -1, -1
+            half3 c1 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zy).rgb; //  1, -1
+            half3 c2 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.xw).rgb; // -1,  1
+            half3 c3 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + d.zw).rgb; //  1,  1
+
+            half3 c = ( c0 + c1 + c2 + c3 ) / 4;
+            return half4( highTex + c * ( 1 + _Density ), 1) / ( 1 + ( _Density * 0.735));
+        #endif
+	}
+
+	// Final composition
+	half4 FragComposition(VaryingsDefault i) : SV_Target
+	{
+		half3 c0 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord).rgb;
+		half3 c1 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord).rgb;
+		half3 c2 = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord).rgb;
+		half3 c3 = SAMPLE_TEXTURE2D(_HighTex, sampler_HighTex, i.texcoord).rgb;
+		half3 cf = (c0 + c1 + c2);
+
+        return half4( lerp( c3, c0 * _Intensity, clamp( Depth(i.texcoord.xy), 0, _Blend) ), 1 );
+	}
+
+	ENDHLSL
+
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+		Pass
+		{
+			HLSLPROGRAM
+			#pragma vertex VertDefault
+			#pragma fragment FragPrefilter
+			ENDHLSL
+		}
+		Pass
+		{
+			HLSLPROGRAM
+			#pragma vertex VertDefault
+			#pragma fragment FragDownsample
+			ENDHLSL
+		}
+		Pass
+		{
+			HLSLPROGRAM
+			#pragma vertex VertDefault
+			#pragma fragment FragUpsample
+			ENDHLSL
+		}
+		Pass
+		{
+			HLSLPROGRAM
+			#pragma vertex VertDefault
+			#pragma fragment FragComposition
+			ENDHLSL
+		}
+	}
+}
